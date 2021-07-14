f35b8c29e0d7b4ab84711321e9924a8fe2ea37ee
ufaboy <39663655+ufaboy@users.noreply.github.com>
2021-07-14 13:11:27 +0000

    Deploying to master from @ ufaboy/ufaboy.github.io@4663cf540a1bdf173dce26e9c76ffe10ed389cd0 🚀

---
+++ precache-manifest.040ff3df31b9005789fb2f60b3d01ac3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1269a0e16fc08d96f3c1",
+    "url": "/css/app.11bb298c.css"
+  },
+  {
+    "revision": "ef85f69c21d5d7ed0d97e21c1581d7be",
+    "url": "/img/Logo.svg"
+  },
+  {
+    "revision": "32bb6a1786c709df600b777144c8c9f3",
+    "url": "/img/Space-BG.jpg"
+  },
+  {
+    "revision": "9373cef007951672261cd4357a3cb636",
+    "url": "/img/Space-BG_blur.jpg"
+  },
+  {
+    "revision": "66fc2b73df2aaa37321aadd5bb689c6d",
+    "url": "/img/gitlab.svg"
+  },
+  {
+    "revision": "dd96644d93c480cb69cc2bd89762498e",
+    "url": "/img/lib-preview.png"
+  },
+  {
+    "revision": "d657d538b9d2551a60e1f3657e35ec26",
+    "url": "/img/photo.png"
+  },
+  {
+    "revision": "50a7a5a337ba15b640ee7d63f74bb352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1269a0e16fc08d96f3c1",
+    "url": "/js/app.4a237b97.js"
+  },
+  {
+    "revision": "cd1a79bec6cde4208492",
+    "url": "/js/chunk-vendors.098ed964.js"
+  },
+  {
+    "revision": "868168eb8e747b39acf9a0e9ecb4d342",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4a4b3afd5ab8c4ab963a977a58b217b0",
+    "url": "/video/lib-prev_2.webm"
+  }
+]);
